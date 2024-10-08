a858d7a768d6864a7afc4c67f0d4d67df252ba5b
juantot9 <juaaratri@gmail.com>
2024-10-08 13:08:34 +0200

    Add html to formatting

--- init.lua
+++ init.lua
@@ -722,6 +722,7 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
